6041a2cb3e8a6d0024d3eb05f6da97cb1d434dd8
André Barão <soneca.ab@hotmail.com>
2022-02-07 09:25:21 -0300

    Filtro de casados

--- alterar.js
+++ alterar.js
@@ -1,1 +1,11 @@
+const pessoas = require('./pessoas.json');
 const fs = require('fs');
+const posicaoDaPessoa = process.argv[2];
+const nome = process.argv[3];
+const cpf = process.argv[4];
+const casado = (process.argv[5] == "1");
+const filhos = process.argv.slice(6);
+const pessoa = {nome, cpf, casado, filhos};
+pessoas.splice(posicaoDaPessoa,1);
+pessoas.push(pessoa);
+fs.writeFileSync('pessoas.json', JSON.stringify(pessoas, null, 4))
